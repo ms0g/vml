4c1249e503cb14ce919bd053df175b034f10478c
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-08-28 12:32:23 +0300

    feat(all): changed all api

--- include/vml.h
+++ include/vml.h
@@ -5,23 +5,21 @@
 extern "C"
 {
 #endif
-extern float *_vec_add(float *const v1, float *const v2, int len);      // v1 + v2
-extern float *_vec_sub(float *const v1, float *const v2, int len);      // v1 - v2
-extern float *_vec_mul(float *const v1, float *const v2, int len);      // v1 * v2
-extern float *_vec_div(float *const v1, float *const v2, int len);      // v1 / v2
-extern float _vec_dot(float *const v1, float *const v2, int len);       // v1 dot v2
-extern float *_vec3_cross(float *const v1, float *const v2, int len);   // v1 x v2
-extern float *_vec_max(float *const v1, float *const v2, int len);      // max(v1, v2)
-extern float *_vec_min(float *const v1, float *const v2, int len);      // min(v1, v2)
-extern float *_vec_sadd(float *const v1, const int scalar, int len);    // v1 + scalar
-extern float *_vec_ssub(float *const v1, const int scalar, int len);    // v1 - scalar
-extern float *_vec_smul(float *const v1, const int scalar, int len);    // v1 * scalar
-extern float *_vec_sdiv(float *const v1, const int scalar, int len);    // v1 / scalar
-extern float _vec_len(float *const v1, int len);                        // length of v1
-extern float *_vec_norm(float *const v1, int len);                      // normalize v1
-extern float *_vec_neg(float *const v1, int len);                       // -v1
-extern float *_vec_abs(float *const v1, int len);                       // |v1|
-
+extern void _vec_add(float* result, float* v1, float* v2, int len);         // v1 + v2
+extern void _vec_sub(float* result, float* v1, float* v2, int len);         // v1 - v2
+extern void _vec_mul(float* result, float* v1, float* v2, int len);         // v1 * v2
+extern void _vec_div(float* result, float* v1, float* v2, int len);         // v1 / v2
+extern void _vec_max(float* result, float* v1, float* v2, int len);         // max(v1, v2)
+extern void _vec_min(float* result, float* v1, float* v2, int len);         // min(v1, v2)
+extern void _vec_sadd(float* result, float* v1, int scalar, int len);       // v1 + scalar
+extern void _vec_ssub(float* result, float* v1, int scalar, int len);       // v1 - scalar
+extern void _vec_smul(float* result, float* v1, int scalar, int len);       // v1 * scalar
+extern void _vec_sdiv(float* result, float* v1, int scalar, int len);       // v1 / scalar
+extern void _vec_neg(float* result, float* v1, int len);                    // -v1
+extern void _vec_abs(float* result, float* v1, int len);                    // |v1|
+extern void _vec_norm(float* result, float* v1, int len);                   // normalize v1
+extern float _vec_len(float* v1, int len);                                  // length of v1
+extern float _vec_dot(float* v1, float* v2, int len);                       // v1 dot v2
 #ifdef __cplusplus
 }
 #endif
